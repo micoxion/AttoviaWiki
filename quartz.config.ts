fcb3062990a3b6e1d83f51a4b23f655dac9ad030
micoxion <mightygamermic@gmail.com>
2026-05-23 19:41:01 -0700

    Quartz sync: May 23, 2026, 7:41 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Caveat Brush",
+        header: "Macondo",
         body: "Macondo",
         code: "IBM Plex Mono",
       },
